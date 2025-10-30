3b6dfbe763226676b944a6770a01d1b419d31390
Brandon Tran <branjtran@gmail.com>
2025-10-29 17:38:52 -0700

    added positional arguments

--- expense_tracker.py
+++ expense_tracker.py
@@ -10,15 +10,19 @@ def add_expense():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('add', help='add an expense')  #positional argument
-    parser.add_argument('--description', type=str, help = 'description of expense')    #optional argument
+    parser.add_argument('add', help='add an expense')  #positional arguments
+    parser.add_argument('update', help='update an existing expense')
+    parser.add_argument('delete', help='delete an existing expense')
+    parser.add_argument('view', help='view all existing expenses')
+    parser.add_argument('summarize', help='summarize all expenses')
+    
+    parser.add_argument('--description', type=str, help = 'description of expense')    #optional arguments
     parser.add_argument('--amount', type=int, help = 'amount of expense')
     args = parser.parse_args()
 
     #functionality below
-
-
-    print(f"Hi! Thanks for entering your name.")
+    if args.add:
+        print('You want to add an expense?')
 
 if __name__ == '__main__':
     main()
