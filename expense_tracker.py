084a87e92d701896561b58db1c05f1bd31573233
Brandon Tran <branjtran@gmail.com>
2025-10-31 18:42:32 -0700

    added wip function

--- expense_tracker.py
+++ expense_tracker.py
@@ -4,14 +4,17 @@ import argparse
 parser = argparse.ArgumentParser()
 
 parser.add_argument('action', help='choose from the following actions: add, update, delete, view, or summarize') #positional argument
-parser.add_argument('--description', default='no description given', type=str, help = 'description of expense')    #optional arguments
-parser.add_argument('--amount', default=0.00, type=float, help = 'amount of expense')
+parser.add_argument('--description', default='no description given', type=str, help = 'description of expense') #optional arguments
+parser.add_argument('--amount', default=NULL, type=float, help = 'amount of expense')
 args = parser.parse_args()
 
 
 expenses = []
 def add_expense(expenses):
     expenses.add[parser.description]
+
+def check_for_data():
+    return  #WIP
 
 def main():
 
