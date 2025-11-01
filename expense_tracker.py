c71a78cb8319a8e0c69ccb6c894dd8a21c9cf458
Brandon Tran <branjtran@gmail.com>
2025-10-31 17:13:05 -0700

    conditionals for each feature in main()

--- expense_tracker.py
+++ expense_tracker.py
@@ -14,28 +14,25 @@ args = parser.parse_args()
 
 
 expenses = []
-def add_expense(expenses[]):
+def add_expense(expenses):
     expenses.add[parser.description]
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('add', help='add an expense')  #positional arguments
-    parser.add_argument('update', help='update an existing expense')
-    parser.add_argument('delete', help='delete an existing expense')
-    parser.add_argument('view', help='view all existing expenses')
-    parser.add_argument('summarize', help='summarize all expenses')
-    
-    parser.add_argument('--description', type=str, help = 'description of expense')    #optional arguments
-    parser.add_argument('--amount', type=int, help = 'amount of expense')
-    args = parser.parse_args()
 
     #functionality 
     if args.add:
         if args.description & args.amount:
-            
+            return
         else:
             print("The 'add' argument requires the '--description' and '--amount' optional arguments to add a new expense.")
-        
+    if args.update:
+        return
+    if args.delete:
+        return
+    if args.view:
+        return
+    if args.summarize:
+        return    
 
 
 if __name__ == '__main__':
