606de8cf6578a4d2a977171b9234781331cb341c
Brandon Tran <branjtran@gmail.com>
2025-10-31 18:31:05 -0700

    added default values to optional arguments

--- expense_tracker.py
+++ expense_tracker.py
@@ -3,17 +3,9 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('action', help='action to add, update, delete, view, or summarize')
-"""
-parser.add_argument('add', help='add an expense')  #positional arguments
-parser.add_argument('update', help='update an existing expense')
-parser.add_argument('delete', help='delete an existing expense')
-parser.add_argument('view', help='view all existing expenses')
-parser.add_argument('summarize', help='summarize all expenses')
-"""
-    
-parser.add_argument('--description', metavar='no description given', type=str, help = 'description of expense')    #optional arguments
-parser.add_argument('--amount', metavar=0.00, type=float, help = 'amount of expense')
+parser.add_argument('action', help='choose from the following actions: add, update, delete, view, or summarize') #positional argument
+parser.add_argument('--description', default='no description given', type=str, help = 'description of expense')    #optional arguments
+parser.add_argument('--amount', default=0.00, type=float, help = 'amount of expense')
 args = parser.parse_args()
 
 
