421fa74b7fba642518b1431723f0e62f28479d4b
Brandon Tran <branjtran@gmail.com>
2025-10-31 22:06:47 -0700

    comments and funcs

--- expense_tracker.py
+++ expense_tracker.py
@@ -4,24 +4,29 @@ from datetime import date
 
 parser = argparse.ArgumentParser()
 
+
 parser.add_argument('action', help='choose from the following actions: add, update, delete, view, or summarize') #positional argument
-parser.add_argument('--date', default=date.today(), help='date of expense (YYYY/MM/DD), current date used if not specified')
+parser.add_argument('--date', default=date.today(), help='date of expense (YYYY-MM-DD), current date used if not specified')
 parser.add_argument('--description', default='no description given', type=str, help = 'description of expense') #optional arguments
 parser.add_argument('--amount', default=0.00, type=float, help = 'amount of expense')
 args = parser.parse_args()
 
 expenses = []
-def edit_data(action, expense): #(integer, list)
+def edit_data(action, expense): #(str, list), add returns int id
+    return #WIP
+
+def add_date(input):
+    
     return
 
 def check_for_data():
-    return  #WIP
+    return  #WIP checks JSON
 
 def main():
     #functionality 
     if args.action == "add":
         edit_data(args.action, [args.date, args.description, args.amount])
-        print(f"added {args.description} and ${args.amount}")
+        print(f"added expense: date:{args.date} desc:{args.description}  amount:${args.amount}")
         
    
 
