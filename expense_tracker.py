61ca3990b2ab113acfa193affeace0119bba0e0c
Brandon Tran <branjtran@gmail.com>
2025-10-31 19:03:43 -0700

    import date from datetime, added argument for date

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,26 +1,27 @@
 import sys
 import argparse
+from datetime import date
 
 parser = argparse.ArgumentParser()
 
 parser.add_argument('action', help='choose from the following actions: add, update, delete, view, or summarize') #positional argument
+parser.add_argument('--date', default=date.today(), help='date of expense, current date used if not specified')
 parser.add_argument('--description', default='no description given', type=str, help = 'description of expense') #optional arguments
-parser.add_argument('--amount', default=NULL, type=float, help = 'amount of expense')
+parser.add_argument('--amount', default=0.00, type=float, help = 'amount of expense')
 args = parser.parse_args()
 
-
 expenses = []
-def add_expense(expenses):
+def edit_data(action, expense): #(integer, list)
     expenses.add[parser.description]
 
 def check_for_data():
     return  #WIP
 
 def main():
-
     #functionality 
     if args.action == "add":
-        print(f"added {args.description} and {args.amount}")
+        edit_data(args.action, [args.description, args.amount])
+        print(f"added {args.description} and ${args.amount}")
         
    
 
