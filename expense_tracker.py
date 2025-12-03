8817f088b809c90fde1b0703e1093d8573bd46e6
Brandon Tran <branjtran@gmail.com>
2025-12-02 18:29:16 -0700

    changed for loop in list_data() to a for loop using enumerate. this accurately prints ID's even if there's dupes

--- expense_tracker.py
+++ expense_tracker.py
@@ -116,8 +116,7 @@ def delete_data(expenses): #delete expense by popping list
 
 def list_data(expenses):
     print(f"{'ID':<3} {'Date':<10} {'Description':<40} {'Amount':<6}")
-    for e in expenses:
-        id = expenses.index(e) + 1
+    for id, e in enumerate(expenses, start=1):
         print(f"{id:<3} {e['date']:<10} {e['description']:<40} {e['amount']:<6}")
     return
 
