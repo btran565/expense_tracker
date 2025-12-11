06824f169111eb9359d3b951ba221bf15626ea43
Brandon Tran <branjtran@gmail.com>
2025-12-10 18:23:49 -0700

    making parent interface for actions

--- tools/action_parent.py
+++ tools/action_parent.py
@@ -1,3 +1,12 @@
 class Action():
-    def __init__(self, args):
+    def __init__(self):
+        """no attributes, left empty"""
+        pass
+
+    def validate():
+        """checks for all valid command line arguments, previously all in check_args.py"""
+        pass
+    
+    def run():
+        """action functionality"""
         pass
--- tools/list.py
+++ tools/list.py
@@ -1,4 +1,9 @@
-def list_data(expenses):
+
+
+def validate(self):
+    pass
+
+def run(expenses):
     print(f"{'ID':<3} {'Date':<10} {'Description':<40} {'Amount':<6}")
     for id, e in enumerate(expenses, start=1):
         print(f"{id:<3} {e['date']:<10} {e['description']:<40} {e['amount']:<6}")
